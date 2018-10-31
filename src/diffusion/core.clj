09e9c719e6c9e962905b859dfb073a91f1ec2231
hpcjkour <hpcjkour@hlrs.de>
2018-10-31 16:08:35 +0100

    Move all functions for writing results to io.clj

--- src/diffusion/core.clj
+++ src/diffusion/core.clj
@@ -2,13 +2,12 @@
   (:require [loom.graph :as g]
             [loom.attr :as a]
             [loom.io :as io]
-            [clojure.java.io :as jio]
-            [clojure.data.csv :as csv]
             [loom.gen :as gen]
             [oz.core :as oz]
             [ministrants.core :as m]
             [anglican.core :refer [doquery]]
-            [diffusion.model :refer [diffusion-query initialize-graph]])
+            [diffusion.model :refer [diffusion-query initialize-graph]]
+            [diffusion.io :as dio])
   (:gen-class))
 
 
@@ -100,6 +99,8 @@
 (def samples (doall (take 1000 (doquery :smc diffusion-query [g3] :number-of-particles 10))))
 ;(first samples)
 ;(count (g/nodes g))
+
+(dio/write-seasons! samples #(m/from-result % [:history :n-green]) "ngreen2.csv")
 
 (defn from-maps
   "Extracts all vals for key(s) ks from a coll of maps. ks must be given as
@@ -211,90 +212,9 @@
 ;
 ;(dashboard samples)
 
-(defmulti #^{:private true} data-for-single-season (fn [query-result f sim-id] (sequential? f)))
-
-(defmethod data-for-single-season false [query-result f sim-id]
-  (let
-    [weeks (range (count (m/from-result query-result [:history :n-green])))
-     cases (f query-result)
-     sim-ids (repeat (count weeks) sim-id)]
-
-    (partition 3
-               (interleave weeks cases sim-ids))))
-
-
-(defmethod data-for-single-season true [query-result f sim-id]
-  (let
-    [weeks (range (count (m/from-result query-result [:history :n-green])))
-     sim-ids (repeat (count weeks) sim-id)
-     cases (map #(%1 query-result) f)]
-
-    (as-> cases v
-          (apply interleave v)
-          (partition (count cases) v)
-          (interleave weeks v sim-ids)
-          (flatten v)
-          (partition (+ (count cases) 2) v))))
-
-(defn write-seasons!
-  "Takes output generated from anglican and writes data for seasons generated
-  by getter-fn to outfile. An additional header can be specified. getter-fn can
-  be a single function or a vector of functions, which can be used to simply
-  collect data from the output or to compute values based on them. Note that
-  these functions have to operate on a single sample, since they are called
-  recursively inside a loop."
-  [samples getter-fn outfile & header]
-  (letfn
-    [(csv-data [samples]
-       (loop [coll []
-              from-query samples
-              n 0]
-
-         (if (not (seq from-query))
-           coll
-
-           (let
-             [single-sample (first from-query)
-              csv-dat (data-for-single-season single-sample getter-fn n)]
-
-             (recur (apply conj coll csv-dat)
-                    (rest from-query)
-                    (inc n))))))]
-
-    (with-open [writer (jio/writer outfile)]
-      (when header
-        (csv/write-csv writer header))
-      (csv/write-csv writer (csv-data samples)))))
-
-;(write-seasons! samples #(m/from-result % [:history :n-green]) "ngreen.csv")
-;(m/from-result (first samples) [:history :n-green])
-;(data-for-single-season (first samples) #(m/from-result % [:history :n-green] ) 0)
-;(def seasons (m/from-results samples [:history :n-green]))
-;(def time-without
-;  (-> samples
-;      (m/from-results [:history :graph])
-;      (times-in-gs)
-;      (flatten)
-;      ))
 
 ;(oz/v! (histo-spec time-without))
 ;(oz/v! (season-plot-spec samples))
-;
-;(defn write-decisions! [decs outfile]
-;  (with-open [writer (jio/writer outfile)]
-;    (csv/write-csv writer decs)))
-
-;(def decision-samples (doall (take 1000 (doquery :smc decision-dist-query [20] :number-of-particles 100))))
-;(def decisions (m/from-results decision-samples [:decisions]))
-;(take 4 decisions)
-;(map + (first decisions) (second decisions))
-;(def red (reduce (fn [one two] (map + one two)) (first decisions) (rest decisions)))
-;
-;(defn format-decisions-vega
-;  [decisions]
-;  (as-> decisions v
-;        (reduce (fn [one two] (map + one two)) (first v) (rest v))
-;        (map (fn [key val] {:n key :p val}) (range (count v)) v)))
 ;
 ;(format-decisions-vega decisions)
 ;
@@ -324,4 +244,3 @@
 ;
 ;
 ;(oz/v! (decision-spec decisions) )
-;(write-decisions! (format-decisions-csv decisions) "decisions.csv")
---
+++ src/diffusion/io.clj
@@ -0,0 +1,70 @@
+(ns diffusion.io
+  (:require [ministrants.core :as m]
+            [clojure.java.io :as jio]
+            [clojure.data.csv :as csv]))
+
+(defmulti #^{:private true} data-for-single-season (fn [query-result f sim-id] (sequential? f)))
+
+(defmethod data-for-single-season false [query-result f sim-id]
+  (let
+    [weeks (range (count (m/from-result query-result [:history :n-green])))
+     cases (f query-result)
+     sim-ids (repeat (count weeks) sim-id)]
+
+    (partition 3
+               (interleave weeks cases sim-ids))))
+
+
+(defmethod data-for-single-season true [query-result f sim-id]
+  (let
+    [weeks (range (count (m/from-result query-result [:history :n-green])))
+     sim-ids (repeat (count weeks) sim-id)
+     cases (map #(%1 query-result) f)]
+
+    (as-> cases v
+      (apply interleave v)
+      (partition (count cases) v)
+      (interleave weeks v sim-ids)
+      (flatten v)
+      (partition (+ (count cases) 2) v))))
+
+(defn write-seasons!
+  "Takes output generated from anglican and writes data for seasons generated
+  by getter-fn to outfile. An additional header can be specified. getter-fn can
+  be a single function or a vector of functions, which can be used to simply
+  collect data from the output or to compute values based on them. Note that
+  these functions have to operate on a single sample, since they are called
+  recursively inside a loop."
+  [samples getter-fn outfile & header]
+  (letfn
+    [(csv-data [samples]
+       (loop [coll []
+              from-query samples
+              n 0]
+
+         (if (not (seq from-query))
+           coll
+
+           (let
+             [single-sample (first from-query)
+              csv-dat (data-for-single-season single-sample getter-fn n)]
+
+             (recur (apply conj coll csv-dat)
+                    (rest from-query)
+                    (inc n))))))]
+
+    (with-open [writer (jio/writer outfile)]
+      (when header
+        (csv/write-csv writer header))
+      (csv/write-csv writer (csv-data samples)))))
+
+;(m/from-result (first samples) [:history :n-green])
+;(data-for-single-season (first samples) #(m/from-result % [:history :n-green] ) 0)
+;(def seasons (m/from-results samples [:history :n-green]))
+;(def time-without
+;  (-> samples
+;      (m/from-results [:history :graph])
+;      (times-in-gs)
+;      (flatten)
+;      ))
+
