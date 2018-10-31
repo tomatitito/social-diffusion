92ed1b525b96e5a94a3d8c89f2024f991c41641d
hpcjkour <hpcjkour@hlrs.de>
2018-10-31 15:41:28 +0100

    Move all model functions to model.clj

--- src/diffusion/core.clj
+++ src/diffusion/core.clj
@@ -6,8 +6,9 @@
             [clojure.data.csv :as csv]
             [loom.gen :as gen]
             [oz.core :as oz]
-            [ministrants.core :as m])
-  (:use [anglican [core :exclude [-main]] runtime emit])
+            [ministrants.core :as m]
+            [anglican.core :refer [doquery]]
+            [diffusion.model :refer [diffusion-query initialize-graph]])
   (:gen-class))
 
 
@@ -75,142 +76,28 @@
         (apply g/graph v)))
 
 
-(defn initialize-graph
-  "Add attributes :green? and :time-without to nodes of g. Initialize all
-  with zero and set :green? of specified proportion of nodes to 1."
-  [g proportion]
-  (let
-    [n-nodes (count (g/nodes g))
-     n-adopters (int (* proportion n-nodes))
-     adopters (repeatedly n-adopters #(rand-nth (range n-nodes)))]
-    (-> g
-        (a/add-attr-to-all :green? 0)
-        (a/add-attr-to-all :time-without 0)
-        (a/add-attr-to-nodes :green? 1 adopters))))
-
-
-(defn count-attr-for-neighbours
-  "Sum up values for attribute attr for all neighbours of node in g."
-  [g node attr]
-  (reduce #(+ %1 (a/attr g %2 attr)) 0 (g/successors g node)))
-
-
-(defn collect-attr-in-graph
-  [g attr]
-  (reduce #(conj %1 (a/attr g %2 attr)) [] (g/nodes g)))
-
-
-(defn count-attr-in-graph
-  [g attr]
-  (reduce #(+ %1 (a/attr g %2 attr)) 0 (g/nodes g)))
-
-
-(defn count-green-neighbours
-  [g node]
-  (count-attr-for-neighbours g node :green?))
-
-
-(defdist decision-dist
-  [g node]
-  [n-neighbours (count (g/successors g node))
-   n-green (count-green-neighbours g node)
-   ratio (/ n-green n-neighbours)]
-  (sample* [this]
-    (if (zero? ratio)
-      (let [param (sample* (uniform-continuous 0.01 0.1))]
-        (sample* (bernoulli param)))
-      (sample* (bernoulli (* 0.1 ratio))))))
-
-
-(with-primitive-procedures [decision-dist]
-  (defquery decision-dist-query [max-neighbours]
-    (let [ns (range (inc max-neighbours))
-          *sample (fn [green-neighbours]
-                    (let [ratio (/ green-neighbours max-neighbours)]
-                      (if (zero? ratio)
-                        (let [param (sample (uniform-continuous 0.01 0.1))]
-                          (sample* (bernoulli param)))
-                        (sample (bernoulli (* 0.1 ratio))))))]
-      {:decisions (reduce (fn [coll val] (conj coll (*sample val))) [] ns)})))
-
-
-(with-primitive-procedures
-  [count-green-neighbours g/successors a/attr a/add-attr decision-dist]
-  (defm look-around
-    "Decision function for the diffusion model. Every node in the graph looks
-    at his neighbours and counts how many of them are already green. This number
-    is used as a parameter for a probabilistic decision to buy a green car."
-        [g node]
-        (if (pos? (attr g node :green?))
-          g
-          (let
-            [time (attr g node :time-without)
-             ;n-neighbours (count (successors g node))
-             ;green-neighbours (count-green-neighbours g node)
-             ;;; new param to make decision harder
-             ;param (* 0.001 (/ green-neighbours n-neighbours))
-
-             ;; use custom dist
-             green? (sample (decision-dist g node))
-             ]
-
-            (if (pos? green?)
-              (add-attr g node :green? 1)
-              (add-attr g node :time-without (inc time)))))))
-
-
-(with-primitive-procedures
-  [g/nodes]
-  (defm diffusion-step
-        [g]
-        (reduce #(look-around %1 %2) g (nodes g))))
-
-
-(with-primitive-procedures
-  [count-attr-in-graph]
-  (defm run-sim
-        [t-max g]
-        (loop [t 0
-               graph g
-               n-green []]
-          (if (= t t-max)
-            {:n-green n-green :graph graph}
-
-            (recur (inc t)
-                   (diffusion-step graph)
-                   (conj n-green (count-attr-in-graph graph :green?)))))))
-
-
-(with-primitive-procedures
-  [count-green-neighbours count-attr-for-neighbours count-attr-in-graph collect-attr-in-graph]
-  (defquery
-    diffusion-query [graph]
-    (let
-      [t 50
-       history (run-sim t graph)
-       ]
-      {:history history})))
-
-
 (def adj-list {0 [1 4] 1 [0 2 3] 2 [1 3 4] 3 [1 2] 4 [0 2 5 6] 5 [4 6] 6 [4 5]})
 
 
-(def g
-  (-> path
-      (create-graph-from-snap-edge-list )
-      (initialize-graph 0.01)))
-
-(def g2
-  (->
-    (gen-newman-watts (g/graph) 1000 10 0.3)
-    (initialize-graph 0.1)))
-
+;(def g
+;  (-> path
+;      (create-graph-from-snap-edge-list )
+;      (initialize-graph 0.01)))
+;
+;(def g2
+;  (->
+;    (gen-newman-watts (g/graph) 1000 10 0.3)
+;    (initialize-graph 0.1)))
+;
 (def g3
   (->
     (gen-newman-watts (g/graph) 10 2 0.1)
     (initialize-graph 0.1)))
 
-(def samples (doall (take 1000 (doquery :smc diffusion-query [g2] :number-of-particles 1000))))
+(def gn (gen-newman-watts (g/graph) 100 3 0.3))
+(io/dot gn "testgraph.dot")
+
+(def samples (doall (take 1000 (doquery :smc diffusion-query [g3] :number-of-particles 10))))
 ;(first samples)
 ;(count (g/nodes g))
 
@@ -379,62 +266,62 @@
         (csv/write-csv writer header))
       (csv/write-csv writer (csv-data samples)))))
 
-(write-seasons! samples #(m/from-result % [:history :n-green]) "ngreen.csv")
+;(write-seasons! samples #(m/from-result % [:history :n-green]) "ngreen.csv")
 ;(m/from-result (first samples) [:history :n-green])
 ;(data-for-single-season (first samples) #(m/from-result % [:history :n-green] ) 0)
 ;(def seasons (m/from-results samples [:history :n-green]))
-(def time-without
-  (-> samples
-      (m/from-results [:history :graph])
-      (times-in-gs)
-      (flatten)
-      ))
+;(def time-without
+;  (-> samples
+;      (m/from-results [:history :graph])
+;      (times-in-gs)
+;      (flatten)
+;      ))
 
-(oz/v! (histo-spec time-without))
-(oz/v! (season-plot-spec samples))
+;(oz/v! (histo-spec time-without))
+;(oz/v! (season-plot-spec samples))
+;
+;(defn write-decisions! [decs outfile]
+;  (with-open [writer (jio/writer outfile)]
+;    (csv/write-csv writer decs)))
 
-(defn write-decisions! [decs outfile]
-  (with-open [writer (jio/writer outfile)]
-    (csv/write-csv writer decs)))
-
-(def decision-samples (doall (take 1000 (doquery :smc decision-dist-query [20] :number-of-particles 100))))
-(def decisions (m/from-results decision-samples [:decisions]))
-(take 4 decisions)
-(map + (first decisions) (second decisions))
-(def red (reduce (fn [one two] (map + one two)) (first decisions) (rest decisions)))
-
-(defn format-decisions-vega
-  [decisions]
-  (as-> decisions v
-        (reduce (fn [one two] (map + one two)) (first v) (rest v))
-        (map (fn [key val] {:n key :p val}) (range (count v)) v)))
-
-(format-decisions-vega decisions)
-
-(defn format-decisions-csv
-  [decisions]
-  (as-> decisions v
-        (reduce (fn [one two] (map + one two)) (first v) (rest v))
-        (map #(/ % 1000.0) v)                                ;to get probabilities
-        (zipmap (range (count v)) v)
-        (partition 2 v)
-        (flatten v)
-        (partition 2 v)))
-
-(format-decisions-csv decisions)
-
-(oz/v! (histo-spec
-         (reduce (fn [one two] (map + one two)) (first decisions) (rest decisions))
-         ))
-
-(defn decision-spec
-  [decisions]
-  {:data     {:values (format-decisions-vega decisions)}
-   :mark     "bar"
-   :encoding {
-              :x {:field :n :type "ordinal"}
-              :y {:field :p :type "quantitative"}}})
-
-
-(oz/v! (decision-spec decisions) )
-(write-decisions! (format-decisions-csv decisions) "decisions.csv")
+;(def decision-samples (doall (take 1000 (doquery :smc decision-dist-query [20] :number-of-particles 100))))
+;(def decisions (m/from-results decision-samples [:decisions]))
+;(take 4 decisions)
+;(map + (first decisions) (second decisions))
+;(def red (reduce (fn [one two] (map + one two)) (first decisions) (rest decisions)))
+;
+;(defn format-decisions-vega
+;  [decisions]
+;  (as-> decisions v
+;        (reduce (fn [one two] (map + one two)) (first v) (rest v))
+;        (map (fn [key val] {:n key :p val}) (range (count v)) v)))
+;
+;(format-decisions-vega decisions)
+;
+;(defn format-decisions-csv
+;  [decisions]
+;  (as-> decisions v
+;        (reduce (fn [one two] (map + one two)) (first v) (rest v))
+;        (map #(/ % 1000.0) v)                                ;to get probabilities
+;        (zipmap (range (count v)) v)
+;        (partition 2 v)
+;        (flatten v)
+;        (partition 2 v)))
+;
+;(format-decisions-csv decisions)
+;
+;(oz/v! (histo-spec
+;         (reduce (fn [one two] (map + one two)) (first decisions) (rest decisions))
+;         ))
+;
+;(defn decision-spec
+;  [decisions]
+;  {:data     {:values (format-decisions-vega decisions)}
+;   :mark     "bar"
+;   :encoding {
+;              :x {:field :n :type "ordinal"}
+;              :y {:field :p :type "quantitative"}}})
+;
+;
+;(oz/v! (decision-spec decisions) )
+;(write-decisions! (format-decisions-csv decisions) "decisions.csv")
---
+++ src/diffusion/model.clj
@@ -0,0 +1,121 @@
+(ns diffusion.model
+  (:require [loom.graph :as g]
+            [loom.attr :as a])
+  (:use [anglican runtime emit]))
+
+
+(defn initialize-graph
+  "Add attributes :green? and :time-without to nodes of g. Initialize all
+  with zero and set :green? of specified proportion of nodes to 1."
+  [g proportion]
+  (let
+    [n-nodes (count (g/nodes g))
+     n-adopters (int (* proportion n-nodes))
+     adopters (repeatedly n-adopters #(rand-nth (range n-nodes)))]
+    (-> g
+        (a/add-attr-to-all :green? 0)
+        (a/add-attr-to-all :time-without 0)
+        (a/add-attr-to-nodes :green? 1 adopters))))
+
+
+(defn count-attr-for-neighbours
+  "Sum up values for attribute attr for all neighbours of node in g."
+  [g node attr]
+  (reduce #(+ %1 (a/attr g %2 attr)) 0 (g/successors g node)))
+
+
+(defn collect-attr-in-graph
+  [g attr]
+  (reduce #(conj %1 (a/attr g %2 attr)) [] (g/nodes g)))
+
+
+(defn count-attr-in-graph
+  [g attr]
+  (reduce #(+ %1 (a/attr g %2 attr)) 0 (g/nodes g)))
+
+
+(defn count-green-neighbours
+  [g node]
+  (count-attr-for-neighbours g node :green?))
+
+
+(defdist decision-dist
+  [g node]
+  [n-neighbours (count (g/successors g node))
+   n-green (count-green-neighbours g node)
+   ratio (/ n-green n-neighbours)]
+  (sample* [this]
+    (if (zero? ratio)
+      (let [param (sample* (uniform-continuous 0.01 0.1))]
+        (sample* (bernoulli param)))
+      (sample* (bernoulli (* 0.1 ratio))))))
+
+
+(with-primitive-procedures [decision-dist]
+  (defquery decision-dist-query [max-neighbours]
+    (let [ns (range (inc max-neighbours))
+          *sample (fn [green-neighbours]
+                    (let [ratio (/ green-neighbours max-neighbours)]
+                      (if (zero? ratio)
+                        (let [param (sample (uniform-continuous 0.01 0.1))]
+                          (sample* (bernoulli param)))
+                        (sample (bernoulli (* 0.1 ratio))))))]
+      {:decisions (reduce (fn [coll val] (conj coll (*sample val))) [] ns)})))
+
+
+(with-primitive-procedures
+  [count-green-neighbours g/successors a/attr a/add-attr decision-dist]
+  (defm look-around
+    "Decision function for the diffusion model. Every node in the graph looks
+    at his neighbours and counts how many of them are already green. This number
+    is used as a parameter for a probabilistic decision to buy a green car."
+    [g node]
+    (if (pos? (attr g node :green?))
+      g
+      (let
+        [time (attr g node :time-without)
+         ;n-neighbours (count (successors g node))
+         ;green-neighbours (count-green-neighbours g node)
+         ;;; new param to make decision harder
+         ;param (* 0.001 (/ green-neighbours n-neighbours))
+
+         ;; use custom dist
+         green? (sample (decision-dist g node))
+         ]
+
+        (if (pos? green?)
+          (add-attr g node :green? 1)
+          (add-attr g node :time-without (inc time)))))))
+
+
+(with-primitive-procedures
+  [g/nodes]
+  (defm diffusion-step
+    [g]
+    (reduce #(look-around %1 %2) g (nodes g))))
+
+
+(with-primitive-procedures
+  [count-attr-in-graph]
+  (defm run-sim
+    [t-max g]
+    (loop [t 0
+           graph g
+           n-green []]
+      (if (= t t-max)
+        {:n-green n-green :graph graph}
+
+        (recur (inc t)
+               (diffusion-step graph)
+               (conj n-green (count-attr-in-graph graph :green?)))))))
+
+
+(with-primitive-procedures
+  [count-green-neighbours count-attr-for-neighbours count-attr-in-graph collect-attr-in-graph]
+  (defquery
+    diffusion-query [graph]
+    (let
+      [t 50
+       history (run-sim t graph)
+       ]
+      {:history history})))
