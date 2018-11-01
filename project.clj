667419065e6640a5499d10b04ee78df3c95aae37
hpcjkour <hpcjkour@hlrs.de>
2018-11-01 19:34:19 +0100

    Add command line options and parse them im -main

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [aysylu/loom "1.0.2-SNAPSHOT"]
                  [org.clojure/data.csv "0.1.3"]
                  [anglican "1.0.0"]
-                 [ministrants "0.1.0-SNAPSHOT"]]
+                 [ministrants "0.1.0-SNAPSHOT"]
+                 [org.clojure/tools.cli "0.4.1"]]
   :main ^:skip-aot diffusion.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/diffusion/core.clj
+++ src/diffusion/core.clj
@@ -6,14 +6,9 @@
             [anglican.core :refer [doquery]]
             [diffusion.model :refer [diffusion-query initialize-graph]]
             [diffusion.io :as dio]
-            [diffusion.gen :refer :all])
+            [diffusion.gen :refer :all]
+            [clojure.tools.cli :refer [parse-opts]])
   (:gen-class))
-
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
 
 
 ;(def g
@@ -39,3 +34,38 @@
 ;(count (g/nodes g))
 
 (dio/write-seasons! samples #(m/from-result % [:history :n-green]) "ngreen2.csv")
+
+(def cli-opts
+  [["-g" "--graph-type graph-type" "Type of graph used in simulation"
+    :default "newman-watts"]
+   ["-d" "--degree degree" "(Out-)Degree for nodes in graph"]
+   ["-h" "--phi phi" "Parameter for adding edges to Newman-Watts graph"
+    :parse-fn #(Float/parseFloat %)
+    :default 0.3
+    :validate [#(<= 0.0 % 1.0) "Parameter for adding edges to Newman-Watts graph has to be between 0 and 1"]]
+   ["-n" "--n-samples n-samples" "Number of simulations run"
+    :default 100]
+   ["-N" "--n-nodes n-nodes" "Number of nodes in graph"
+    :default 50]
+   ["-a" "--algorithm algorithm" "Algortithm used for sampling from model"
+    :default :smc
+    :parse-fn #(keyword %)]
+   ["-o" "--outfile"
+    :required "Path to write results to"
+    :id :outfile]])
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (let [parsed-args (parse-opts args cli-opts)
+        graph-type (get-in parsed-args [:options :graph-type])
+        n-nodes (get-in parsed-args [:options :n-nodes])
+        degree (get-in parsed-args [:options :degree])
+        gen-fn (symbol (str "gen-" graph-type))
+        in-graph (if (= graph-type "newman-watts")
+                   (gen-newman-watts (g/graph) n-nodes degree phi))
+        n-samples (get-in parsed-args [:options :n-samples])
+        samples (doall (take n-samples (doquery diffusion-query [in-graph] :number-of-particles 100)))]
+    (println gen-fn)
+    )
+  )
