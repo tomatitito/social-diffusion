2fb73ca930d27dc9a288301326b643c4cb3f189f
hpcjkour <hpcjkour@hlrs.de>
2018-11-01 19:39:48 +0100

    Add function to generate barabsi-albert graph

--- src/diffusion/gen.clj
+++ src/diffusion/gen.clj
@@ -1,5 +1,5 @@
 (ns diffusion.gen
-  (:require [loom.graph :as g]
+  (:require [g :as g]
             [loom.attr :as a]))
 
 
@@ -63,4 +63,43 @@
 
 ;(def path "datasets/data")
 (def path "/home/dusty/dev/python/snappy/edgeList.txt")
+
+
+(defn gen-barabasi-albert
+  "Generate a preferential attachment graph as described in Barabasi
+  and Albert (1999)."
+  ([g num-nodes num-edges seed]
+   (let [rnd (java.util.Random. seed)
+         ;; initialize graph with two connected nodes
+         ;; with equal probability, a new node will attach to
+         ;; either one
+         g-0 (g/add-edges g [0 1])
+         ;; predicate for deciding wether a node
+         ;; should be connected to a new node
+         connect? (fn [g node]
+                    (let [degree-node (count (g/successors g node))
+                          degree-sum (reduce #(+ %1 (count (g/successors g %2))) 0 (nodes g))]
+                      (<= (/ degree-node degree-sum) (.nextDouble rnd))))
+         ;; go through all nodes in g and decide whether
+         ;; they connect to new
+         new-edges (fn [g new]
+                     (for [n (nodes g)
+                           :when (connect? g n)]
+                       [new n]))
+         ;; compute num-edges edges for new in graph g
+         get-new-edges-and-connect (fn [g new num-edges]
+                                     (as-> g v
+                                       (new-edges v new)
+                                       (take num-edges v)
+                                       (filter #(= 2 (count %)) v)
+                                       (apply g/add-edges g v)))
+         ;; two nodes are already in the initialized graph
+         ;; the remaining notes will be added
+         remaining-nodes (range 2 num-nodes)
+         ]
+
+     (reduce #(get-new-edges-and-connect %1 %2 num-edges) g-0 remaining-nodes)))
+  ([g num-nodes num-edges]
+   (gen-barabasi-albert g num-nodes num-edges (System/nanoTime))))
+
 
